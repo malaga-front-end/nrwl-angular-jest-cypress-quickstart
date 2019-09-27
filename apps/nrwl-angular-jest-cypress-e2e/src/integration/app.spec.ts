e83d077bba1fadca9665151228eec4bae08982a6
Miguel Angel Munoz <ma.munoz.alonso@gmail.com>
2019-09-27 09:18:10 +0200

    E2E testing with cypress

--- apps/nrwl-angular-jest-cypress-e2e/src/integration/app.spec.ts
+++ apps/nrwl-angular-jest-cypress-e2e/src/integration/app.spec.ts
@@ -1,18 +1,16 @@
-import { getCountries, getCity } from '../support/app.po';
-
 describe('app', () => {
   beforeEach(() => {
-    cy.server()           // enable response stubbing
+    cy.server();           // enable response stubbing
     cy.route({
       method: 'GET',      // Route all GET requests
-      url: '/countries',    // that have a URL that matches '/users/*'
-      response: [{country: 'Spain', city: 'Madrid'}]        // and force the response to be: []
-    })
-    cy.visit('/')
+      url: '/countries',    // that have a URL that matches '/countries/*'
+      response: [{country: 'Spain', city: 'Madrid'}]        // and force the response to be this one
+    });
+    cy.visit('/');
   });
 
   it('should display city when clicking on country', () => {
-    getCountries().eq(0).click();
-    getCity().contains('Madrid');
+    cy.get('li').eq(0).click();
+    cy.get('h1').contains('Madrid');
   });
 });
--- apps/nrwl-angular-jest-cypress-e2e/src/support/app.po.ts
+++
@@ -1,2 +0,0 @@
-export const getCountries = () => cy.get('ul li');
-export const getCity = () => cy.get('h1');
