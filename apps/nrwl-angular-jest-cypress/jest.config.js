2504d38ad41e4adc4cf911aeecee7b43df9abc64
Miguel Angel Munoz <ma.munoz.alonso@gmail.com>
2019-10-16 16:42:08 +0200

    A new beginning always starts at the end

--- apps/nrwl-angular-jest-cypress/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'nrwl-angular-jest-cypress',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/nrwl-angular-jest-cypress',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
